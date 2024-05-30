7f14840acfccded734598034b6a5faf29144ea61
Emilia Alvarez <Emiiaalvarezib@gmail.com>
2024-05-30 02:43:29 -0300

    Cree el objeto Automóvil con las propiedades marca, modelo y año. Lo contrui mediante una función constructora (se define con la palabra clave class y operador new para crear una instancia de objeto) y utilize la consola para mostrar la información almacenada.

--- 2/1/ProgramacionWeb/Construccion de Objetos/Automovil.js
+++ 2/1/ProgramacionWeb/Construccion de Objetos/Automovil.js
@@ -1,9 +1,15 @@
 //Un objeto Automóvil con las propiedades marca, modelo y año. Construirlo mediante una función constructora y utilizar la consola para mostrar la información almacenada.
 
-var automovil = {
-    marca: 'Ford',
-    modelo: 'F-100',
-    anio: 1979
+class automovil {
+    constructor(marca, modelo, anio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;    
+    }
 }
 
-console.log(automovil.marca);
+var miAuto = new automovil("Ford", "F-100", 1978);
+
+console.log(miAuto.marca);
+console.log(miAuto.modelo);
+console.log(miAuto.anio);
