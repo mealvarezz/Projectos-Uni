9b260c4430d56e41703d1b10b4ee5ec7c73f1290
Emilia Alvarez <Emiiaalvarezib@gmail.com>
2024-05-30 02:59:12 -0300

    Cree una clase Rectangulo con un constructor que reciba alto y ancho y un método calcularArea que devuelva el área del rectángulo (alto x ancho). Cree una instancia de Rectangulo y mostre el área calculada en la consola.

--- 2/1/ProgramacionWeb/Construccion de Objetos/Rectangulo.js
+++ 2/1/ProgramacionWeb/Construccion de Objetos/Rectangulo.js
@@ -1,2 +1,18 @@
 //Una clase Rectangulo con un constructor que reciba alto y ancho. Incorporar un método calcularArea que devuelva el área del rectángulo (alto x ancho). Crear una instancia de Rectangulo y mostrar el área calculada en la consola.
 
+class Rectangulo {
+  constructor(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+  }
+
+  calcularArea() {
+    return this.alto * this.ancho;
+  }
+}
+
+var rectangulo = new Rectangulo(7, 4);
+
+console.log("El área del rectángulo es: " + rectangulo.calcularArea());
+
+
