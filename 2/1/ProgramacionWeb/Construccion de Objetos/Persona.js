f26f38eccbe4effb1efbf0c3315614f8493a818a
Emilia Alvarez <Emiiaalvarezib@gmail.com>
2024-05-30 02:34:04 -0300

    Cree el objeto Persona con las propiedades nombre, edad y ciudad. Y lo construi mediante su inicialización y utilice la consola ra mostrar la información almacenada.

--- 2/1/ProgramacionWeb/Construccion de Objetos/Persona.js
+++ 2/1/ProgramacionWeb/Construccion de Objetos/Persona.js
@@ -1,2 +1,13 @@
 //Un objeto Persona con las propiedades nombre, edad y ciudad. Construirlo mediante su inicialización y utilizar la consola para mostrar la información almacenada.
 
+var persona = {
+    nombre: "Maria",
+    edad: 43,
+    ciudad: "Rawson"
+}
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.ciudad);
+
+
