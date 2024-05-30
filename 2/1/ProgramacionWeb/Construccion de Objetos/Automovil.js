b8c9fa984483f1f74c302933b19c9e1de8befee3
Emilia Alvarez <Emiiaalvarezib@gmail.com>
2024-05-30 08:37:30 -0300

    Lo modifique y li hice en base a la pagina 16 de la unidad V lenguajes scripting

--- 2/1/ProgramacionWeb/Construccion de Objetos/Automovil.js
+++ 2/1/ProgramacionWeb/Construccion de Objetos/Automovil.js
@@ -1,11 +1,9 @@
 //Un objeto Automóvil con las propiedades marca, modelo y año. Construirlo mediante una función constructora y utilizar la consola para mostrar la información almacenada.
 
-class Automovil {
-    constructor(marca, modelo, anio){
+function Automovil(marca, modelo, anio) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;    
-    }
 }
 
 var automovil = new Automovil("Ford", "F-100", 1978);
